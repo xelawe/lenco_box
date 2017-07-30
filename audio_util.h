288c0fc43dddb1a0100ff2671f4ea49d8b21155e
xelawe <alexander@wernhart.net>
2017-07-30 15:26:26 +0200

    delta

---
+++ audio_util.h
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#include <PT2314.h> // https://github.com/andykarpov/PT2314
+
+PT2314 audio;
+
+/**
+   Send tone control values to the PT2314
+https://github.com/andykarpov/PiRadio-OLED/tree/master/ino/src
+*/
+void sendPT2314() {
+  audio.volume(60);
+  audio.bass(0);
+  audio.treble(0);
+  audio.channel(3);
+  //  if (power_on) {
+  audio.loudnessOn();
+  audio.muteOff();
+  //  } else {
+  //    audio.loudnessOff();
+  //    audio.muteOn();
+  //  }
+}
+
+void init_audio( ) {
+  Wire.begin(13, 14);
+
+  audio.init();
+  sendPT2314();
+
+}
+
+<<<<<<< HEAD
+
+=======
+
+>>>>>>> 3b9c9224140ba2a94abf8578b61a21a387c0ccc5
---
+++ lenco_box.ino
@@ -0,0 +1,49 @@
+/*
+   Blink and OTA
+   Turns on the onboard LED on for one second, then off for one second, repeatedly.
+   This uses delay() to pause between LED toggles.
+*/
+#define serdebug
+#ifdef serdebug
+#define DebugPrint(...) {  Serial.print(__VA_ARGS__); }
+#define DebugPrintln(...) {  Serial.println(__VA_ARGS__); }
+#else
+#define DebugPrint(...) { }
+#define DebugPrintln(...) { }
+#endif
+
+#include "tools_wifi.h"
+#include "ota_tool.h"
+#include "audio_util.h"
+
+void setup() {
+#ifdef serdebug
+  Serial.begin(115200);
+#endif
+
+  wifi_init("D1mini");
+  delay(500);
+
+  init_ota("D1mini");
+
+  pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
+
+  init_audio( );
+  delay(1000);                      // wait one second
+}
+
+void loop() {
+
+  check_ota();
+
+  digitalWrite(BUILTIN_LED, HIGH);  // turn on LED with voltage HIGH
+  delay(1000);                      // wait one second
+  digitalWrite(BUILTIN_LED, LOW);   // turn off LED with voltage LOW
+  delay(1000);                      // wait one second
+
+
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 3b9c9224140ba2a94abf8578b61a21a387c0ccc5
